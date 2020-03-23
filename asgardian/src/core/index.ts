16a5e31e8e277a32ffdb362b1720e285622f047a
Guilherme <guilhermebromonschenkel@gmail.com>
2020-03-23 19:29:30 -0300

    chore(rpc): add rpc default setup

--- asgardian/src/core/index.ts
+++ asgardian/src/core/index.ts
@@ -1,6 +1,7 @@
 import Consumer from "@core/consumer"
 import Server from "@core/server"
 import Database from "@core/database"
+import Rpc from "@core/rpc"
 
 class App {
 	static async start(): Promise<void> {
@@ -8,6 +9,7 @@ class App {
 
 		await Database.start()
 		Server.setup()
+		Rpc.setup()
 		await Consumer.process()
 	}
 }
